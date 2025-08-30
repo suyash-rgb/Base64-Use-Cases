77848e2b17835eb7a68de67515715b5065c82a92
Suyash Baoney <suyashbaoney58@gmail.com>
2025-08-30 15:18:34 +0530

    adding initial project setup

---
+++ Base64UseCases/src/main/java/com/suyash/Base64UseCases/Base64UseCasesApplication.java
@@ -0,0 +1,13 @@
+package com.suyash.Base64UseCases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64UseCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Base64UseCasesApplication.class, args);
+	}
+
+}
---
+++ Base64UseCases/src/test/java/com/suyash/Base64UseCases/Base64UseCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.Base64UseCases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64UseCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
